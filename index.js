0e7412a173b381c7810ccc65d5d7b9a62210d1f8
Vedang Naik <vedang.a.naik@gmail.com>
2019-01-20 22:36:27 +0530

    Major restructure to circumvent annoyances in using SearchQuery slot, improved error messages and handling.

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ const langPollyVoices = {
 // JSON list of facts for each supported languages.
 const langFacts = require("./language_facts.json");
 
+
+
+
 /*
 This function makes sure that all the words in a sentence are valid english ones. 
 */
@@ -50,106 +53,118 @@ function validateSentence(sentence) {
 	return true;
 }
 
-const handlers = {
-	/*
-	Handles the LaunchIntent and starts a session.
-	*/
-	'LaunchRequest': function () {
-		this.emit(":ask", "Welcome to Language Learning with Alexa.");
-	},
-
-	/*
-	Handles the Translate Intent. Steps:
+/*
+Handles the translation for each individual language's TranslateIntent.
+Steps:
 	1. Gets the target language and phrase from the user.
 	2. Sends the phrase with language to Translate.
 	3. Sends the phrase, translatation and language to DynamoDB for the QuizIntent.
 	4. Sends the translated phrase and language to Polly for text-to-speech.
 	5. Save the speech file to S3 temporarily.
 	6. Speak out the speech file from S3.
-	*/
-	'TranslateIntent': function () {
-		// Make sure that the sentence contains only English words.
-		var sentence = this.event.request.intent.slots.sentence.value;
-		if (!validateSentence(sentence)) {
-			this.emit(":tell", "Your sentence contains some words not found in international English.");
-		}
+*/
+function translateSentence(sentence, language, thisReference) {
+	if (!validateSentence(sentence)) {
+		thisReference.emit(":tell", "Your sentence contains some words not found in international English.");
+	}
 
-		// Ensure that the language is in the list of allowed languages.
-		var language = this.event.request.intent.slots.language.value;
-		language = language.toLowerCase();
-		if (!validateSentence(language)) {
-			this.emit(":tell", "This language is invalid.");
-		} else if (language == "english") {
-			this.emit(":tell", "Translating from English to English is not necessary.");
-		} else if (langShortForms[language] == undefined) {
-			this.emit(":tell", "This language is not supported by this skill.");
-		}
-
-		// 1. Send async call to Translate API.
-		new Promise((resolve, reject) => {
-			new AWS.Translate().translateText({
-				"SourceLanguageCode": "en",
-				"TargetLanguageCode": langShortForms[language],
-				"Text": sentence
-			}, (err, translatedText) => {
-				if (err) { reject(err); }
+	// 1. Send async call to Translate API.
+	new Promise((resolve, reject) => {
+		new AWS.Translate().translateText({
+			"SourceLanguageCode": "en",
+			"TargetLanguageCode": langShortForms[language],
+			"Text": sentence
+		}, (err, translatedText) => {
+			if (err) {
+				thisReference.emit(":tell", "There was a translation error, please try again.");
+				reject(err);
+			}
+			else { resolve(translatedText); }
+		});
+	// 2. If Translate succeeds, send translated text to QuizTable to quiz the user on later.
+	}).then(translatedText => {
+		return new Promise((resolve, reject) => {
+			new AWS.DynamoDB().putItem({
+				TableName: "AlexaLanguageLearn-QuizTable",
+				Item: {
+					"allqt_sentence_id": {"S": uuidv4() },
+					"allqt_language": {"S": language},
+					"allqt_sentence": {"S": sentence},
+					"allqt_translation": {"S": translatedText["TranslatedText"] },
+					"allqt_user": {"S": thisReference.event.session.user.userId}
+				}
+			}, function(err, translatedText) {
+				if (err) {
+					thisReference.emit(":tell", "A database error occured, please try again.");
+					reject(err);
+				}
 				else { resolve(translatedText); }
 			});
-		// 2. If Translate succeeds, send translated text to QuizTable to quiz the user on later.
-		}).then(translatedText => {
-			return new Promise((resolve, reject) => {
-				new AWS.DynamoDB().putItem({
-					TableName: "AlexaLanguageLearn-QuizTable",
-					Item: {
-						"allqt_sentence_id": {"S": uuidv4() },
-						"allqt_language": {"S": language},
-						"allqt_sentence": {"S": sentence},
-						"allqt_translation": {"S": translatedText["TranslatedText"] },
-						"allqt_user": {"S": this.event.session.user.userId}
-					}
-				}, function(err, data) {
-					if (err) { reject(err); }
-					else { resolve(translatedText); }
-				});
-			});
-		// 3. If DynamoDB succeeds, send the same translated text to Polly to speak.
-		}).then(translatedText => {
-			return new Promise((resolve, reject) => {
-				new AWS.Polly({
-					signatureVersion: 'v4',
-					region: 'us-east-1'
-				}).synthesizeSpeech({
-					"Text": translatedText["TranslatedText"],
-					"OutputFormat": "mp3",
-					"VoiceId": langPollyVoices[translatedText["TargetLanguageCode"]]
-				}, (err, translatedSpeech) => {
-					if (err) { reject(err); }
-					else { resolve(translatedSpeech); }
-				});
-			});
-		// 4. If Polly succeeds, save AudioStream to S3 Bucket.
-		}).then(translatedSpeech => {
-			return new Promise((resolve, reject) => {
-				if (translatedSpeech.AudioStream instanceof Buffer) {
-					new AWS.S3().upload({
-						"Bucket": "alexalanguagelearn-bucket",
-						"Key": "temp.mp3",
-						"Body": translatedSpeech.AudioStream
-					}, (err, savedFileInfo) => {
-						if (err) { reject(err); }
-						else { resolve(savedFileInfo); }
-					});
-				} else { reject(err); }
-			});
-		// 5. If S3 filesave succeeds, play audio back to user with SSML.
-		}).then(savedFileInfo => {
-			this.emit(":tell", "The phrase " + sentence + " in " + language + " is: <audio src='" + savedFileInfo["Location"] + "'/>");
-		// An error in any of the above steps is logged here.
-		}).catch((err) => {
-			console.log(err);
-			this.emit(":tell", "There was a translation error.");
 		});
+	// 3. If DynamoDB succeeds, send the same translated text to Polly to speak.
+	}).then(translatedText => {
+		return new Promise((resolve, reject) => {
+			new AWS.Polly({
+				signatureVersion: 'v4',
+				region: 'us-east-1'
+			}).synthesizeSpeech({
+				"Text": translatedText["TranslatedText"],
+				"OutputFormat": "mp3",
+				"VoiceId": langPollyVoices[translatedText["TargetLanguageCode"]]
+			}, (err, translatedSpeech) => {
+				if (err) {
+					thisReference.emit(":tell", "There was a text-to-speech error, please try again.");
+					reject(err);
+				}
+				else { resolve(translatedSpeech); }
+			});
+		});
+	// 4. If Polly succeeds, save AudioStream to S3 Bucket.
+	}).then(translatedSpeech => {
+		return new Promise((resolve, reject) => {
+			if (translatedSpeech.AudioStream instanceof Buffer) {
+				new AWS.S3().upload({
+					"Bucket": "alexalanguagelearn-bucket",
+					"Key": "temp.mp3",
+					"Body": translatedSpeech.AudioStream
+				}, (err, savedFileInfo) => {
+					if (err) {
+						thisReference.emit(":tell", "There was a file save error, please try again.");
+						reject(err);
+					}
+					else { resolve(savedFileInfo); }
+				});
+			} else {
+				thisReference.emit(":tell", "An audio file error occured, please try again.");
+				reject(err);
+			}
+		});
+	// 5. If S3 filesave succeeds, play audio back to user with SSML.
+	}).then(savedFileInfo => {
+		thisReference.emit(":tell", "The phrase " + sentence + " in " + language + " is: <audio src='" + savedFileInfo["Location"] + "'/>");
+	// An error in any of the above steps is logged here.
+	}).catch((err) => {
+		console.log(err);
+		thisReference.emit(":tell", "There was an unknown error during the process, please try again.");
+	});
+}
+
+
+
+
+const handlers = {	
+	'LaunchRequest': function () {
+		this.emit(":ask", "Welcome to Lingua Franca.");
 	},
+
+	'TranslateFrenchIntent': function () { translateSentence(this.event.request.intent.slots.sentence.value, "french", this); },
+	'TranslateGermanIntent': function () { translateSentence(this.event.request.intent.slots.sentence.value, "german", this); },
+	'TranslateSpanishIntent': function () { translateSentence(this.event.request.intent.slots.sentence.value, "spanish", this);	},
+	'TranslateTurkishIntent': function () { translateSentence(this.event.request.intent.slots.sentence.value, "turkish", this);	},
+	'TranslateChineseIntent': function () { translateSentence(this.event.request.intent.slots.sentence.value, "chinese", this);	},
+	'TranslateRussianIntent': function () { translateSentence(this.event.request.intent.slots.sentence.value, "russian", this);	},
+	'TranslateItalianIntent': function () { translateSentence(this.event.request.intent.slots.sentence.value, "italian", this);	},
+	'TranslatePortugueseIntent': function () { translateSentence(this.event.request.intent.slots.sentence.value, "portuguese", this); },
 
 	/*
 	Handles the QuizIntent. Steps:
@@ -185,7 +200,11 @@ const handlers = {
 				}, function(err, results) {
 					if (err) { reject(err); }
 					// Pick a random one using some maths.
-					else { resolve(results["Items"][Math.floor(Math.random() * results["Count"])]); }
+					else {
+						if (results["Count"] == 0) {
+							this.emit(":tell", "You have never asked for any translations in this language. Please try another one.");
+						}
+						resolve(results["Items"][Math.floor(Math.random() * results["Count"])]); }
 				});
 			}).then(randomItem => {
 				this.attributes.randomItem = randomItem;
@@ -246,10 +265,18 @@ const handlers = {
 		this.emit(":tell", factBank[Math.floor(Math.random() * factBank.length)]);
 	},
 
-
-	'AMAZON.HelpIntent': function () {},
-	'AMAZON.CancelIntent': function () {},
-	'AMAZON.StopIntent': function () {}
+	'AMAZON.FallbackIntent': function() {
+		this.emit(":tell", "You have asked for something Lingua Franca cannot help you with. Please say 'help' to view Lingua Franca's services.");
+	},
+	'AMAZON.HelpIntent': function () {
+		this.emit(":tell", "Lingua Franca is a skill to help English speakers learn other languages. Say 'translate to language the phrase so-and-so' to translate your phrase to one of French, Spanish, German, Italian, Russian, Chinese, Portuguese and Turkish. Say 'quiz me on language' to be questioned about the meaning of a word you've asked before. Speak your answer afterwards and the skill will give you confirmation. Say 'give me a fact on language' to learn a random fact about that language.");
+	},
+	'AMAZON.CancelIntent': function () {
+		this.emit(":tell", "Your current operation has been stopped.");
+	},
+	'AMAZON.StopIntent': function () {
+		this.emit(":tell", "Thank you for using Lingua Franca. Come again!");
+	}
 };
 
 exports.handler = function (event, context, callback) {
@@ -258,13 +285,3 @@ exports.handler = function (event, context, callback) {
 	alexa.registerHandlers(handlers);
 	alexa.execute();
 };
-
-/*
-List of TODOs
-1. Get a better dictionary.
-2. Handle all built-in intents.
-3. Error handling for the database, all slots, etc.
-4. Utterances for TranslateIntent
-5. Change the name of the skill.
-6. Fix bug in QuizIntent.
-*/
